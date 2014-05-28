be05a68d3e20991be0a747c78ba362ed6c499572
jasonklein <jasonklein@gmail.com>
2014-05-28 15:02:42 +0100

    Add compile_cards_array function; test passing.

--- jason_banks/w9d3/luhn-app/luhn.rb
+++ jason_banks/w9d3/luhn-app/luhn.rb
@@ -1,10 +1,12 @@
 class Luhn
 
-  attr_reader :name, :amount
+  attr_reader :name, :amount, :cards_array
 
   def initialize(name)
     @name = name
   end
+
+  # this method is not currently useful, as I need to review the RSpec docs about printing something to the console and storing the response from the user.
 
   def set_amount_of_cards(amount)
     @amount = 0
@@ -15,6 +17,13 @@ class Luhn
       "Amount must be a number equal to 10 or less." # To manage the for loop.
     else
       @amount = amount
+    end
+  end
+
+  def compile_cards_array(array)
+    @cards_array = []
+    array.each do |number|
+      @cards_array << number
     end
   end
 
--- jason_banks/w9d3/luhn-app/spec/luhn_spec.rb
+++ jason_banks/w9d3/luhn-app/spec/luhn_spec.rb
@@ -19,11 +19,20 @@ describe Luhn do
       luhn.set_amount_of_cards(10)
       expect(luhn.amount).to eq 10
 
-      luhn.set_amount_of_cards(11)
       expect(luhn.set_amount_of_cards(11)).to include "Amount must be a number equal to 10 or less."
       expect(luhn.amount).to eq 0
     end
   end
+
+  describe '#compile_cards_array' do
+    it 'accepts the card numbers and places in an array' do
+      array = ['4111111111111111', '4111111111111', '4012888888881881', '378282246310005']
+      luhn.compile_cards_array(array)
+      expect(luhn.cards_array).to eq ['4111111111111111', '4111111111111', '4012888888881881', '378282246310005']
+    end
+  end
+
+
 
 
 
