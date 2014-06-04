ff33f65757ff4431c8df6416a583700c07e3399a
jasonklein <jasonklein@gmail.com>
2014-06-04 16:56:31 +0100

    Add functions and tess.

---
+++ jason_banks/w10d3/grocery-code-test/checkout.rb
@@ -0,0 +1,67 @@
+class Checkout
+
+  attr_reader :basket, :items, :total_price
+
+  def initialize(basket=[])
+    @basket = basket
+    @items = {SR1: 0, FR1: 0, CF1: 0}
+    @total_price = 0.0
+  end
+
+  def determine_basket_composition
+    basket.each do |item|
+      if item.to_sym == :SR1
+        items[:SR1] += 1
+      elsif item.to_sym == :FR1
+        items[:FR1] += 1
+      else
+        items[:CF1] += 1
+      end
+    end
+  end
+
+  def scan_items
+    @total_price += price_strawberries(items[:SR1]) + price_fruit_tea(items[:FR1]) + total_price + price_coffee(items[:CF1])
+  end
+
+  def price_strawberries(amount)
+    current_price = 5.00
+    discount_price = 4.50
+    if amount >= 3 
+      price = amount * discount_price
+    else
+      price = amount * current_price
+    end
+    price
+  end
+
+  def price_fruit_tea(amount)
+    current_price = 3.11
+    bogo_amount = amount - (amount / 2)
+    price = bogo_amount * current_price
+    price
+  end
+
+  def price_coffee(amount)
+    current_price = 11.23
+    price = amount * current_price
+    price
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
--- jason_banks/w10d3/grocery-code-test/spec/checkout_spec.rb
+++ jason_banks/w10d3/grocery-code-test/spec/checkout_spec.rb
@@ -3,6 +3,31 @@ require_relative '../checkout'
 
 describe Checkout do
 
+  let (:basket) {basket = ['SR1', 'SR1', 'FR1', 'SR1']}
+  let (:co) {Checkout.new(basket)}
+
+  describe ".new" do
+    it "creates a new Checkout object that takes a basket of goods" do
+      expect(co).to_not be nil
+      expect(co.basket).to eq ['SR1', 'SR1', 'FR1', 'SR1']
+    end
+  end
+
+  describe "#determine_basket_composition" do
+    it "determines the basket composition" do
+      co.determine_basket_composition
+      expect(co.items).to include SR1: 3, FR1: 1, CF1: 0
+    end
+  end
+
+  describe "#scan_items" do
+    it "scans the items and returns a total price" do
+      co.determine_basket_composition
+      co.scan_items
+      expect(co.total_price).to eq 16.61
+    end
+  end
+
 
 
 
